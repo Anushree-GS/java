5ffa29ae071d6c7d36aa7d1ebfa9eceea1e3f509
Anushree-GS <anushreesganachari@gmail.com>
2024-10-16 20:52:23 +0530

    primitivetypes

---
+++ primitivedatatype.java
@@ -0,0 +1,167 @@
+public class Primitivedatatype{
+public static void main(String[]args)
+{
+byte age = 60;
+byte monthsInYear = 12;
+byte alphabets = 26;
+byte numberOfDistrictsInState = 31;
+byte numberOfPensInBox = 10;
+byte fingersInAHand = 10;
+byte numberOfSticksInAMatchbox = 30;
+byte numberOfKeysInAKeyboard = 85;
+byte numberOfStatesInIndia = 28;
+byte numberOfEyesForAHumanbeing = 2;
+System.out.println(age);
+System.out.println(monthsInYear);
+System.out.println(alphabets);
+System.out.println(numberOfDistrictsInState);
+System.out.println(numberOfPensInBox);
+System.out.println(fingersInAHand);
+System.out.println(numberOfSticksInAMatchbox);
+System.out.println(numberOfKeysInAKeyboard);
+System.out.println(numberOfStatesInIndia );
+System.out.println(numberOfEyesForAHumanbeing);
+
+short numberOfTaluksInKarnataka  = 236;
+short numberOfMlasInKarnataka = 224;
+short numberOfCountriesInWorld = 195;
+short numberOfDistrictsInIndia = 800;
+short numberOfBonesInHumanbody = 206;
+short numberOfWBCs = 10000;
+short numberOfRoomsInVidhansoudha = 172;
+short numbeOfVillagesInKarnataka = 29340;
+short numbeOfHobliesInKarnataka = 747;
+short numberOfStudentsInAClass = 100;
+System.out.println(numberOfTaluksInKarnataka);
+System.out.println(numberOfMlasInKarnataka);
+System.out.println(numberOfCountriesInWorld);
+System.out.println(numberOfDistrictsInIndia);
+System.out.println(numberOfBonesInHumanbody);
+System.out.println(numberOfWBCs);
+System.out.println(numberOfRoomsInVidhansoudha);
+System.out.println(numbeOfVillagesInKarnataka);
+System.out.println(numbeOfHobliesInKarnataka);
+System.out.println(numberOfStudentsInAClass);
+
+int registerNumber = 367889;
+int numberOfBooksInLibrariy = 46767887;
+int populationOfOurCity = 455667656;
+int priceOfATwoWheeler = 1000000;
+int rentOfHome = 66667;
+int salaryOfIASOfficer = 100000;
+int priceOfANanoCar = 300000;
+System.out.println(registerNumber);
+System.out.println(numberOfBooksInLibrariy);
+System.out.println(populationOfOurCity);
+System.out.println(priceOfATwoWheeler);
+System.out.println(rentOfHome);
+System.out.println(salaryOfIASOfficer);
+System.out.println(priceOfANanoCar);
+
+long phoneNumber = 3456218954l;
+long adharNumber = 335677854379l;
+long populationOfWorld = 96378444555437834l;
+long numberOfVechiles = 445678674598l;
+long ticketNumber = 23443455628l;
+long studentsSatsNumber = 8737629847472l;
+long sspId = 2019054737903l;
+long numberOfPeoplesInIndia = 46656767707565l;
+long bankAccountNumber = 563278326422l;
+System.out.println(phoneNumber);
+System.out.println(adharNumber);
+System.out.println(populationOfWorld);
+System.out.println(numberOfVechiles);
+System.out.println(ticketNumber);
+System.out.println(studentsSatsNumber);
+System.out.println(sspId);
+System.out.println(numberOfPeoplesInIndia);
+System.out.println(bankAccountNumber);
+ 
+float height = 5.6f;
+float piValue = 3.14f;
+float cgpa = 7.9f;
+float gravity = 9.8f;
+float negativeNumbers = -6.8f;
+float possitiveNumbers = 5.788f;
+float temperature = 34.3f;
+float pkValueOfAceticAcid = 4.76f;
+float meltingPointOfAcetamide = 16.6f;
+float moleValueOfWater = 18.0152f;  
+System.out.println(height);
+System.out.println(piValue);
+System.out.println(cgpa);
+System.out.println(gravity);
+System.out.println(negativeNumbers);
+System.out.println(possitiveNumbers);
+System.out.println(temperature);
+System.out.println(pkValueOfAceticAcid);
+System.out.println(meltingPointOfAcetamide);
+System.out.println(moleValueOfWater);
+
+double bankBalance = 5000.235d;
+double speedOfLight = 2.997946d;
+double quantityOfSeeds = 44.45d;
+double divisionOfTwoVariables = 46678.988d;
+double voltage = 12.7d;
+double spaceLeft = 767.8888d;
+double aQuantityOfGold = 488.79d;
+double possitveNumbers = 78678.77d;
+System.out.println(bankBalance);
+System.out.println(speedOfLight);
+System.out.println(quantityOfSeeds);
+System.out.println(divisionOfTwoVariables);
+System.out.println(voltage);
+System.out.println(spaceLeft);
+System.out.println(aQuantityOfGold);
+System.out.println(possitveNumbers);
+
+
+boolean isItOk = true;
+boolean isItGood = false;
+boolean isItBetter = true;
+boolean isItAvailable = true;
+boolean areYouFine = true;
+boolean isItRainning = true;
+boolean wasItMatters = false;
+boolean itWasAGoodThing = true;
+boolean isItEassy = false;
+boolean isItBeautiful = true;
+System.out.println(isItOk);
+System.out.println(isItGood);
+System.out.println(isItBetter);
+System.out.println(isItAvailable);
+System.out.println(areYouFine);
+System.out.println(isItRainning);
+System.out.println(wasItMatters);
+System.out.println(itWasAGoodThing);
+System.out.println(isItEassy);
+System.out.println(isItBeautiful);
+
+
+
+
+char attendanceOfStudents ='P';
+char myGrade ='A';
+char initial ='J';
+char numberOfStudentsPresent ='P';
+char numberOfStudentsAbsent ='A';
+char sections ='c';
+char numberOfStudentsPass ='P';
+char numberOfStudentsFail ='F';
+char availableOfTickets ='S';
+char block ='N';
+System.out.println(attendanceOfStudents);
+System.out.println( myGrade);
+System.out.println(initial);
+System.out.println(numberOfStudentsPresent);
+System.out.println(numberOfStudentsAbsent);
+System.out.println(sections);
+System.out.println(numberOfStudentsPass);
+System.out.println(numberOfStudentsFail);
+System.out.println(availableOfTickets);
+System.out.println(block);
+}
+}
+
+
+
