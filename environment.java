27044bcb88b0ad27768e6277e90fb5e82210339a
Anushree-GS <anushreesganachari@gmail.com>
2024-10-18 19:07:52 +0530

    Add files via upload

---
+++ environment.java
@@ -0,0 +1,28 @@
+public class environment{
+
+
+	public static void main (String[]args){
+		nature();
+		nature();
+	operations();
+	nature();
+	operations();
+
+
+	}
+
+
+
+    public static void nature(){
+	System.out.println("Nature is the source of all true knowledge.");
+	System.out.println("The beauty of nature lies in its freshness,warm sun.");
+	System.out.println("To reduce stress and axiety we need to spend some valuable time with nature.");
+
+	}
+	public static void operations(){
+		
+		System.out.println("mathematical operations are very important.");
+		System.out.println("add,multiplication,sub,division.");
+	}
+}
+	
