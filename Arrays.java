d44756291fe52dfcf5928e72d0b005102370ae52
Anushree-GS <anushreesganachari@gmail.com>
2024-11-12 19:31:44 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,327 @@
+public class Arrays{
+
+	public static void main(String[] args){
+
+		byte[] alphabets = {1,2,3,4,5,6,7,8,9,10};
+		System.out.println(alphabets[0]);
+		System.out.println(alphabets[1]);
+		System.out.println(alphabets[2]);
+		System.out.println(alphabets[3]);
+		System.out.println(alphabets[4]);
+		System.out.println(alphabets[5]);
+		System.out.println(alphabets[6]);
+		System.out.println(alphabets[7]);
+		System.out.println(alphabets[8]);
+		System.out.println(alphabets[9]);
+
+
+		byte[]albhabet = new byte[10];
+		albhabet[0]=10;
+		albhabet[1]=11;
+		albhabet[2]=12;
+		albhabet[3]=13;
+		albhabet[4]=14;
+		albhabet[5]=15;
+		albhabet[6]=16;
+		albhabet[7]=17;
+		albhabet[8]=18;
+		albhabet[9]=19;
+
+		System.out.println(albhabet[0]);
+		System.out.println(albhabet[1]);
+		System.out.println(albhabet[2]);
+		System.out.println(albhabet[3]);
+		System.out.println(albhabet[4]);
+		System.out.println(albhabet[5]);
+		System.out.println(albhabet[6]);
+		System.out.println(albhabet[7]);
+		System.out.println(albhabet[8]);
+		System.out.println(albhabet[9]);
+
+		short[] peoples = {4667,783,32000,500,429,961,1143,2234,9453,7535};
+		System.out.println(peoples[0]);
+		System.out.println(peoples[1]);
+		System.out.println(peoples[2]);
+		System.out.println(peoples[3]);
+		System.out.println(peoples[4]);
+		System.out.println(peoples[5]);
+		System.out.println(peoples[6]);
+		System.out.println(peoples[7]);
+		System.out.println(peoples[8]);
+		System.out.println(peoples[9]);
+
+		short[]people = new short[10];
+		people[0]=7765;
+		people[1]=8068;
+		people[2]=4530;
+		people[3]=9532;
+		people[4]=16578;
+		people[5]=7549;
+		people[6]=7766;
+		people[7]=4065;
+		people[8]=3765;
+		people[9]=21765;
+
+		System.out.println(people[0]);
+		System.out.println(people[1]);
+		System.out.println(people[2]);
+		System.out.println(people[3]);
+		System.out.println(people[4]);
+		System.out.println(people[5]);
+		System.out.println(people[6]);
+		System.out.println(people[7]);
+		System.out.println(people[8]);
+		System.out.println(people[9]);	
+	
+
+      int[] values = {45,55,87,35,90,54,87,71,98,85};
+      System.out.println(values[0]);
+      System.out.println(values[1]);
+      System.out.println(values[2]);
+      System.out.println(values[3]);
+      System.out.println(values[4]);
+      System.out.println(values[5]);
+      System.out.println(values[6]);
+      System.out.println(values[7]);
+      System.out.println(values[8]);
+      System.out.println(values[9]);
+
+      values[0]=46;
+      System.out.println(values[0]);
+
+
+      int[]value = new int[10];
+      value[0]=1;
+      value[1]=7657;
+      value[2]=19659;
+      value[3]=16789;
+      value[4]=4501;
+      value[5]=3331;
+      value[6]=8641;
+      value[7]=18604;
+      value[8]=8761;
+      value[9]=1896;
+      
+
+      System.out.println(value[0]);
+      System.out.println(value[1]);
+      System.out.println(value[2]);
+      System.out.println(value[3]);
+      System.out.println(value[4]);
+      System.out.println(value[5]);
+      System.out.println(value[6]);
+      System.out.println(value[7]);
+      System.out.println(value[8]);
+      System.out.println(value[9]);
+      
+      long[] adharno = {267478644376l,654321907598l,876543214567l,98765432579l,876543212345l,878790098753l,908643535646l,897649519979l,346375297504l,437588423461l};
+      System.out.println(adharno[0]);
+      System.out.println(adharno[1]);
+      System.out.println(adharno[2]);
+      System.out.println(adharno[3]);
+      System.out.println(adharno[4]);
+      System.out.println(adharno[5]);
+      System.out.println(adharno[6]);
+      System.out.println(adharno[7]);
+      System.out.println(adharno[8]);
+      System.out.println(adharno[9]);
+
+      long[] phoneno = new long[10];
+      phoneno[0]=5678923456l;
+      phoneno[1]=6779686868l;
+      phoneno[2]=6547964789l;
+      phoneno[3]=9987877778l;
+      phoneno[4]=6768868979l;
+      phoneno[5]=7756788999l;
+      phoneno[6]=9833763637l;
+      phoneno[7]=8008039543l;
+      phoneno[8]=4456799008l;
+      phoneno[9]=5775789646l;
+      System.out.println(phoneno[0]);
+      System.out.println(phoneno[1]);
+      System.out.println(phoneno[2]);
+      System.out.println(phoneno[3]);
+      System.out.println(phoneno[4]);
+      System.out.println(phoneno[5]);
+      System.out.println(phoneno[6]);
+      System.out.println(phoneno[7]);
+      System.out.println(phoneno[8]);
+      System.out.println(phoneno[9]);
+      
+      float[]cgpa={8.9f,9.0f,9.9f,9.6f,7.9f,8.8f,6.9f,9.2f,4.7f,8.5f};
+      System.out.println(cgpa[0]);
+      System.out.println(cgpa[1]);
+      System.out.println(cgpa[2]);
+      System.out.println(cgpa[3]);
+      System.out.println(cgpa[4]);
+      System.out.println(cgpa[5]);
+      System.out.println(cgpa[6]);
+      System.out.println(cgpa[7]);
+      System.out.println(cgpa[8]);
+      System.out.println(cgpa[9]);
+
+      float[]height = new float[10];
+      height[0]=5.8f;
+      height[1]=9.5f;
+      height[2]=7.8f;
+      height[3]=8.1f;
+      height[4]=7.7f;
+      height[5]=8.8f;
+      height[6]=5.9f;
+      height[7]=5.8f;
+      height[8]=7.0f;
+      height[9]=5.6f;
+      System.out.println(height[0]);
+      System.out.println(height[1]);
+      System.out.println(height[2]);
+      System.out.println(height[3]);
+      System.out.println(height[4]);
+      System.out.println(height[5]);
+      System.out.println(height[6]);
+      System.out.println(height[7]);
+      System.out.println(height[8]);
+      System.out.println(height[9]);
+      
+
+      double[] bankBalance ={200.323d,34245.2424d,3424124.2424d,454442.4424d,3453.33333d,3424455.445d,335522.535d,8975443.45d,467866.677d,65788,873d};
+      System.out.println(bankBalance[0]);
+      System.out.println(bankBalance[1]);
+      System.out.println(bankBalance[2]);
+      System.out.println(bankBalance[3]);
+      System.out.println(bankBalance[4]);
+      System.out.println(bankBalance[5]);
+      System.out.println(bankBalance[6]);
+      System.out.println(bankBalance[7]);
+      System.out.println(bankBalance[8]);
+      System.out.println(bankBalance[9]);
+      
+      double[] divisionOfTwoNO = new double[10];
+      divisionOfTwoNO[0]=5678.797d;
+      divisionOfTwoNO[1]=5678.7943d;
+      divisionOfTwoNO[2]=68.797d;
+      divisionOfTwoNO[3]=568.797d;
+      divisionOfTwoNO[4]=5678.77d;
+      divisionOfTwoNO[5]=5678.797d;
+      divisionOfTwoNO[6]=5678.7978d;
+      divisionOfTwoNO[7]=566678.79721d;
+      divisionOfTwoNO[8]=45678.797d;
+      divisionOfTwoNO[9]=7678.797d;
+
+      System.out.println(divisionOfTwoNO[0]);
+      System.out.println(divisionOfTwoNO[1]);
+      System.out.println(divisionOfTwoNO[2]);
+      System.out.println(divisionOfTwoNO[3]);
+      System.out.println(divisionOfTwoNO[4]);
+      System.out.println(divisionOfTwoNO[5]);
+      System.out.println(divisionOfTwoNO[6]);
+      System.out.println(divisionOfTwoNO[7]);
+      System.out.println(divisionOfTwoNO[8]);
+      System.out.println(divisionOfTwoNO[9]);
+      
+
+      boolean[] isItAvailable ={true,true,false,false,false,true,true,false,false,false,};
+       System.out.println(isItAvailable[0]);
+       System.out.println(isItAvailable[1]);
+       System.out.println(isItAvailable[2]);
+       System.out.println(isItAvailable[3]);
+       System.out.println(isItAvailable[4]);
+       System.out.println(isItAvailable[5]);
+       System.out.println(isItAvailable[6]);
+       System.out.println(isItAvailable[7]);
+       System.out.println(isItAvailable[8]);
+
+       boolean[] isItGood = new boolean[10];
+       isItGood[0]=true;
+       isItGood[1]=true;
+       isItGood[2]=true;
+       isItGood[3]=true;
+       isItGood[4]=true;
+       isItGood[5]=true;
+       isItGood[6]=true;
+       isItGood[7]=true;
+       isItGood[8]=true;
+       isItGood[9]=true;
+
+       System.out.println(isItGood[0]);
+       System.out.println(isItGood[1]);
+       System.out.println(isItGood[2]);
+       System.out.println(isItGood[3]);
+       System.out.println(isItGood[4]);
+       System.out.println(isItGood[5]);
+       System.out.println(isItGood[6]);
+       System.out.println(isItGood[7]);
+       System.out.println(isItGood[8]);
+       System.out.println(isItGood[9]);
+
+
+       char[] myGrade ={'A','B','A','B','A','B','A','B','A','B'};
+       System.out.println(myGrade[0]);
+       System.out.println(myGrade[1]);
+       System.out.println(myGrade[2]);
+       System.out.println(myGrade[3]);
+       System.out.println(myGrade[4]);
+       System.out.println(myGrade[5]);
+       System.out.println(myGrade[6]);
+       System.out.println(myGrade[7]);
+       System.out.println(myGrade[8]);
+       System.out.println(myGrade[9]);
+
+       char[] section = new char[10];
+       section[0]='A';
+       section[1]='B';
+       section[2]='A';
+       section[3]='B';
+       section[4]='A';
+       section[5]='B';
+       section[6]='A';
+       section[7]='A';
+       section[8]='B';
+       section[9]='A';
+
+       System.out.println(section[0]);
+       System.out.println(section[1]);
+       System.out.println(section[2]);
+       System.out.println(section[3]);
+       System.out.println(section[4]);
+       System.out.println(section[5]);
+       System.out.println(section[6]);
+       System.out.println(section[7]);
+       System.out.println(section[8]);
+       System.out.println(section[9]);
+       
+
+
+
+
+
+
+       
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+
+
+
+
+
+
+
+
+
+  }
+}
